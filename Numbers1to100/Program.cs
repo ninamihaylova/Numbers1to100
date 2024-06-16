f76ce281ba5441ae742bd5e7e33a04c33f4cf8c4
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:28:17 +0300

    Initial commit

---
+++ Numbers1to100/Program.cs
@@ -0,0 +1,17 @@
+﻿class Numbers1to100
+{
+    public static void Main(string[] args)
+    {
+
+
+        int i = 1;
+        {
+            while (i <= 100)
+
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
